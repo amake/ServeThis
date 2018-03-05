5991d35dd200626e963a76fb0e096eacfc263a6e
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-03-05 22:30:13 +0900

    Add log noting server URL, open URL automatically

--- ServeThis.sh
+++ ServeThis.sh
@@ -3,5 +3,13 @@
 cd ../../..
 
 for port in {8000..8100}; do
-    python -m SimpleHTTPServer $port
+    python -m SimpleHTTPServer $port &
+    pid=$!
+    sleep 0.5
+    if ps -p $pid &>/dev/null; then
+        url="http://localhost:${port}"
+        echo "Serving at ${url}..."
+        open $url
+        wait $pid
+    fi
 done
